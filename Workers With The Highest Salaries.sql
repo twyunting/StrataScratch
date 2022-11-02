32580a03838ee1f7a2f33c12bbbdf65bc186bd7b
Yunting Chiu <yc6705a@american.edu>
2022-11-01 22:48:52 -0500

    Create Workers With The Highest Salaries.sql

---
+++ Workers With The Highest Salaries.sql
@@ -0,0 +1,10 @@
+with tmp as(
+
+select worker_title, max(salary) as salary from worker W
+inner join title T
+on W.worker_id = T.worker_ref_id
+group by worker_title
+order by salary desc)
+
+select * from tmp
+where salary = (select max(salary) from worker)
